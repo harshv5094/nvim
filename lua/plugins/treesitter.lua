1323c574a8778b16036a763a5603dac4fdf6ceb2
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2025-08-09 17:47:41 +0530

    feat: adding treesitter support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,1 +1,30 @@
-return {}
+return {
+	"nvim-treesitter/nvim-treesitter",
+	event = "VeryLazy",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"bash",
+				"fish",
+				"typescript",
+				"python",
+				"go",
+				"vimdoc",
+				"query",
+				"markdown",
+				"markdown_inline",
+			},
+			auto_install = true,
+			sync_install = false,
+			highlight = {
+				enabled = true,
+			},
+			indent = {
+				enabled = true,
+			},
+		})
+	end,
+}
