520a4ff9e81bb7460ac3072cd3061cd5055ddbe8
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2025-11-25 18:58:32 +0530

    chore: remove lualine
    I'm going for quick minimal config, so I don't think lualine is
    necessary for me

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -85,14 +85,4 @@ return {
 			},
 		},
 	},
-
-	-- NOTE: Lualine nvim
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			local lualine = require("lualine")
-			lualine.setup({})
-		end,
-	},
 }
