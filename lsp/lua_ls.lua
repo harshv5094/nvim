1ed8ddc063e571e25d27cf2c5f57d1dd3a618fa0
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2025-11-26 19:50:58 +0530

    chore(lua_ls): using @MariaSolOs `lua_ls` settings
    See:
    https://github.com/MariaSolOs/dotfiles/blob/main/.config/nvim/lsp/lua_ls.lua

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -6,18 +6,26 @@
 return {
 	cmd = { "lua-language-server" },
 	filetypes = { "lua" },
-	root_markers = { { ".luarc.json", ".luarc.jsonc" } },
+	root_markers = { ".luarc.json", ".luarc.jsonc" },
+	-- NOTE: These will be merged with the configuration file.
 	settings = {
 		Lua = {
-			diagnostics = {
-				globals = { "vim", "Snacks" },
-			},
-			-- Disable telemetry files
-			telemetry = {
-				enable = false,
+			completion = { callSnippet = "Replace" },
+			-- Using stylua for formatting.
+			format = { enable = false },
+			hint = {
+				enable = true,
+				arrayIndex = "Disable",
 			},
 			runtime = {
 				version = "LuaJIT",
+			},
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME,
+					"${3rd}/luv/library",
+				},
 			},
 		},
 	},
