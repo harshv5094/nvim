a0adc06b4fccac78819369121b6cb417936410eb
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2025-12-05 22:52:22 +0530

    chore: add lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -68,4 +68,15 @@ return {
 			},
 		},
 	},
+
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			theme = "gruvbox",
+		},
+		config = function(_, opts)
+			require("lualine").setup(opts)
+		end,
+	},
 }
