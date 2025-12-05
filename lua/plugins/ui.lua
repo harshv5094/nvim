0a250e868853a5a75efe201a2802f78f0b7518d1
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2025-12-05 18:37:11 +0530

    chore: remove which-key

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -68,21 +68,4 @@ return {
 			},
 		},
 	},
-
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		opts = {
-			preset = "helix",
-		},
-		keys = {
-			{
-				"<leader>?",
-				function()
-					require("which-key").show({ global = false })
-				end,
-				desc = "Buffer Local Keymaps (which-key)",
-			},
-		},
-	},
 }
