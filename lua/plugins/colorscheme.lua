baa27813485a5710567049c5062f2694479ad1eb
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2025-12-07 20:31:51 +0530

    fix: turn off italics in gruvbox
    Not helping in readability

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,15 @@ return {
 	priority = 1000,
 	opts = {
 		transparent_mode = true,
+		undercurl = true,
+		underline = true,
+		italic = {
+			strings = false,
+			emphasis = false,
+			comments = false,
+			operators = false,
+			folds = false,
+		},
 	},
 	config = function(_, opts)
 		require("gruvbox").setup(opts)
