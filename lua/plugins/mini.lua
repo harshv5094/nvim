bc0fe7a22e6dab916716e2eaeb990a11931216c8
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2025-12-26 22:54:56 +0530

    chore(mini): add `mini.pick`
    I love this quick file picker

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -10,4 +10,21 @@ return {
 			require("mini.pairs").setup({})
 		end,
 	},
+
+	{
+		"nvim-mini/mini.pick",
+		event = { "BufReadPre", "BufNewFile" },
+		cmd = "Pick",
+		version = false,
+		config = function()
+			require("mini.pick").setup({})
+		end,
+		keys = {
+			{
+				"<leader>x",
+				"<CMD>Pick files<CR>",
+				"mini.pick -> Files",
+			},
+		},
+	},
 }
