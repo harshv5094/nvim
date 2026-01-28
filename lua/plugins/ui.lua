ebe61e639e5416fa7476951acad50eab679d3384
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-01-28 23:51:51 +0530

    feat(lualine): remove seperator symbols

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -73,7 +73,10 @@ return {
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {
-			theme = "gruvbox",
+			options = {
+				component_separators = { left = "|", right = "|" },
+				section_separators = { left = "", right = "" },
+			},
 			sections = {
 				lualine_a = { { "mode", icon = "" } },
 			},
