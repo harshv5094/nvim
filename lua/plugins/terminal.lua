e06dd7265d5c7b571541dba7b380da9ba161d832
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-01-30 08:43:55 +0530

    feat: add toggleterm for toggling terminal

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,12 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	cmd = "ToggleTerm",
+	keys = {
+		{
+			"<leader>ft",
+			"<CMD>ToggleTerm<CR>",
+			desc = "Toggle Terminal",
+		},
+	},
+}
