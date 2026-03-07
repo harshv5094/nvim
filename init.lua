24e6359796c64c87fbd5752324e292c50d26af38
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-03-07 18:10:33 +0530

    feat: add more options and keymaps

--- init.lua
+++ init.lua
@@ -3,6 +3,27 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Setting up basic options
-vim.o.cursorline = true
-vim.o.relativenumber = true
-vim.o.clipboard = "unnamedplus"
+local opt = vim.o
+
+opt.cursorline = true --Highlighted cursorline
+opt.relativenumber = true -- Relative Line number
+opt.clipboard = "unnamedplus" -- Enable global clipboard
+opt.autoindent = true -- Indent automatically
+opt.ignorecase = true -- Ignore case while searching
+opt.wrap = false -- Turn off wrapping lines (I don't like it)
+opt.splitright = true -- Split window on right
+opt.splitbelow = true -- Split window on below
+opt.swapfile = true -- Use swap file to save unsave backup
+
+-- Netrw Explorer keymap
+vim.keymap.set("n", "<leader>e", "<CMD>Explore<CR>", { desc = "Explore" })
+
+-- Split screen keymaps
+vim.keymap.set("n", "ss", "<CMD>split<CR>")
+vim.keymap.set("n", "sv", "<CMD>vsplit<CR>")
+
+-- Split screen focus navigation
+vim.keymap.set("n", "sh", "<C-W>h")
+vim.keymap.set("n", "sj", "<C-W>j")
+vim.keymap.set("n", "sk", "<C-W>k")
+vim.keymap.set("n", "sl", "<C-W>l")
