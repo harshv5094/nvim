bbcfec1c1b680c4ce7769841d6d5313a06bb7fea
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-03-13 23:33:38 +0530

    feat(nvim/blink.cmp): enable emoji support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,11 @@
 return {
 	{
 		"saghen/blink.cmp",
-    dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+   	dependencies = {
+		 {'L3MON4D3/LuaSnip', version = 'v2.*' },
+			"moyiz/blink-emoji.nvim",
+			"saghen/blink.compat",
+ 		},
 		opts = {
 			keymap = {
 				preset = 'default',
@@ -10,12 +14,24 @@ return {
     		['<CR>'] = { 'accept_and_enter', 'fallback' },
 			},
   		sources = {
-      	default = { 'lsp', 'path', 'snippets', 'buffer' },
+      	default = { 'lsp', 'path', 'snippets', 'buffer', 'emoji' },
+				providers = {
+					emoji = {
+						module = "blink-emoji",
+						name = "Emoji",
+						score_offset = 15, -- Tune by preference
+						opts = { insert = true }, -- Insert emoji (default) or complete its name
+					},
+				},
     	},
 			cmdline = {
 					enabled = false
 			},
 			completion = {
+				documentation = {
+  				auto_show = true,
+  				auto_show_delay_ms = 500,
+				},
 				trigger = {
 					prefetch_on_insert = true
 				},
