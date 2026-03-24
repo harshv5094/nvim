e89d11f09ed5f877c66e723af5363eef1f509500
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-03-24 08:40:14 +0530

    feat(autocmds): add autocmds function from @MariaSolOs

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,3 +9,20 @@ api.nvim_create_autocmd("TextYankPost", {
 	group = highlight_group,
 	pattern = "*",
 })
+
+-- Set up LSP servers.
+vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+	once = true,
+	callback = function()
+		-- Extend neovim's client capabilities with the completion ones.
+		vim.lsp.config("*", { capabilities = require("blink.cmp").get_lsp_capabilities(nil, true) })
+
+		local servers = vim
+			.iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
+			:map(function(file)
+				return vim.fn.fnamemodify(file, ":t:r")
+			end)
+			:totable()
+		vim.lsp.enable(servers)
+	end,
+})
