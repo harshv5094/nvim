65ec8600579d04807aee38fc9d083b1453a60716
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-03-30 19:54:11 +0530

    chore(coding): use keys format from coding.lua

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -3,14 +3,38 @@ return {
 	{
 		"tpope/vim-fugitive",
 		event = "BufReadPre",
-		config = function()
-			vim.keymap.set("n", "<leader>gs", "<CMD>Git status<CR>", { desc = "Git Status" })
-			vim.keymap.set("n", "<leader>ga", ":Git add ", { desc = "Git Add" })
-			vim.keymap.set("n", "<leader>gce", "<CMD>Git commit --edit<CR>", { desc = "Git Commit (edit)" })
-			vim.keymap.set("n", "<leader>gca", "<CMD>Git commit --amend<CR>", { desc = "Git Commit (amend)" })
-			vim.keymap.set("n", "<leader>gp", "<CMD>Git pull --no-edit<CR>", { desc = "Git Pull (No Edit)" })
-			vim.keymap.set("n", "<leader>gP", "<CMD>Git push --force-with-lease<CR>", { desc = "Git Push" })
-		end,
+		keys = {
+			{
+				"<leader>gs",
+				"<CMD>Git status<CR>",
+				desc = "Git status",
+			},
+			{
+				"<leader>ga",
+				":Git add ",
+				desc = "Git status",
+			},
+			{
+				"<leader>gce",
+				"<CMD>Git commit --edit<CR>",
+				desc = "Git commit (edit)",
+			},
+			{
+				"<leader>gca",
+				"<CMD>Git commit --amend<CR>",
+				desc = "Git commit (amend)",
+			},
+			{
+				"<leader>gp",
+				"<CMD>Git pull --no-edit<CR>",
+				desc = "Git pull (no-edit)",
+			},
+			{
+				"<leader>gP",
+				"<CMD>Git push --force-with-lease<CR>",
+				desc = "Git push (force-with-lease)",
+			},
+		},
 	},
 
 	-- NOTE: Diagnostics panel from folke
