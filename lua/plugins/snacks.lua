8ad2a1141696e32abb0c3e722db8c6683b5b1abe
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-04-23 14:43:58 +0530

    feat(snacks): enable zen mode

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"folke/snacks.nvim",
+		cmd = "Snacks",
 		priority = 1000,
 		lazy = false,
 		---@type snacks.Config
@@ -8,6 +9,7 @@ return {
 			statuscolumn = { enabled = true },
 			indent = { enabled = true },
 			lazygit = { enabled = true },
+			zen = { enabled = true },
 		},
 		keys = {
 			{
@@ -16,6 +18,20 @@ return {
 					Snacks.terminal()
 				end,
 				desc = "Toggle Terminal",
+			},
+			{
+				"<leader>z",
+				function()
+					Snacks.zen()
+				end,
+				desc = "Toggle Zen Mode",
+			},
+			{
+				"<leader>Z",
+				function()
+					Snacks.zen.zoom()
+				end,
+				desc = "Toggle Zoom",
 			},
 			{
 				"<leader>gg",
