90d36ef97b76308a2ce9a76a7882a9de0d7624df
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-08-22 19:19:35 +0530

    refactor(lsp.lua): moving mason contents to lsp.lua
    Don't need to specify files for mason, treesitter is different

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -73,4 +73,53 @@ return {
 			fuzzy = { implementation = "lua" },
 		},
 	},
+
+	-- NOTE: Mason
+	{
+		"mason-org/mason.nvim",
+		version = "*", -- Installs the latest stable release of mason
+		cmd = "Mason",
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+		end,
+		keys = {
+			{
+				"<leader>cm",
+				"<CMD>Mason<CR>",
+				{ desc = "Mason" },
+			},
+		},
+	},
+
+	-- NOTE: Auto installer for mason.nvim
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "mason-org/mason.nvim" },
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					-- LSP servers
+					"lua-language-server",
+					"bash-language-server",
+					"yaml-language-server",
+					"taplo",
+
+					-- Formatters / linters
+					"stylua",
+					"shellcheck",
+					"shfmt",
+				},
+				automatic_installation = true,
+				run_on = { "BufReadPre", "VeryLazy" },
+			})
+		end,
+	},
 }
