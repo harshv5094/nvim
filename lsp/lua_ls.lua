3279a5997697172cf105053c152c6fa21d6f7d31
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-08-22 19:36:04 +0530

    chore(lsp/lua_ls): disable some annoying hints

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -16,6 +16,11 @@ return {
 			},
 			hint = {
 				enable = true,
+				setType = false,
+				paramType = true,
+				paramName = "Disable",
+				semicolon = "Disable",
+				arrayIndex = "Disable",
 			},
 		},
 	},
