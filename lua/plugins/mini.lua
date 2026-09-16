8a0a40f4779a2086ced0128d8db64851e75b4195
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-09-16 09:48:09 +0530

    chore(mini-icons): removing this icons library since I'm already using nvim-web-devicons, so I'm removing it as to not add any extra layer

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -18,13 +18,4 @@ return {
 			require("mini.pairs").setup()
 		end,
 	},
-
-	-- NOTE: Mini Icons library
-	{
-		"nvim-mini/mini.icons",
-		version = false,
-		config = function()
-			require("mini.icons").setup()
-		end,
-	},
 }
